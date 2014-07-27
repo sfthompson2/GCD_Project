7e4184de67a56155100432a9b40b89f54bed1e1b
sfthompson2 <sfthompson2@gmail.com>
2014-07-27 14:56:21 -0400

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,55 @@
+Xtrain<-read.table("./data/UCI HAR Dataset/train/X_train.txt")
+Xtest<-read.table("./data/UCI HAR Dataset/test/X_test.txt")
+Ytrain<-read.table("./data/UCI HAR Dataset/train/y_train.txt")
+Ytest<-read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_train<-read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+subject_test<-read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+activity_lables<-read.table("./data/UCI HAR Dataset/activity_labels.txt")
+features<-read.table("./data/UCI HAR Dataset/features.txt")
+
+#----------------Merges the training and the test sets to create one data set.
+Xall<-rbind(Xtrain,Xtest)
+
+#----------------Extracts only the measurements on the mean and standard deviation for each measurement.
+colnames(Xall) <- c(as.character(features[,2]))
+
+Mean<-grep("mean()",colnames(Xall),fixed=TRUE)
+
+SD<-grep("std()",colnames(Xall),fixed=TRUE)
+
+MeanSD<-Xall[,c(Mean,SD)]
+
+#----------------Uses descriptive activity names to name the activities in the data set.
+
+Yall<-rbind(Ytrain,Ytest)
+
+all.activity<-cbind(Yall,MeanSD)
+
+colnames(all.activity)[1] <- "Activity"
+
+#----------------Appropriately labels the data set with descriptive activity names.
+activity_lables[,2]<-as.character(activity_lables[,2])
+
+for(i in 1:length(all.activity[,1])){
+  all.activity[i,1]<-activity_lables[all.activity[i,1],2]
+}
+
+#----------------Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+subject_all<-rbind(subject_train,subject_test)
+
+all<-cbind(subject_all,all.activity)
+
+colnames(all)[1] <- "Subject"
+
+Tidy <- aggregate( all[,3] ~ Subject+Activity, data = all, FUN= "mean" )
+
+for(i in 4:ncol(all)){
+  Tidy[,i] <- aggregate( all[,i] ~ Subject+Activity, data = all, FUN= "mean" )[,3]
+}
+
+colnames(Tidy)[3:ncol(Tidy)] <- colnames(MeanSD)
+
+write.table(Tidy, file = "FinalData.txt")
+
+Final <- read.table("./FinalData.txt")
